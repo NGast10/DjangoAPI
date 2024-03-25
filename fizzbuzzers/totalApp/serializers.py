74aa555647302336b1dd3eac53e1c76b37551f0a
Nyla Gaston <143456031+NGast10@users.noreply.github.com>
2024-03-24 23:33:36 -0700

    Update serializers.py

--- fizzbuzzers/totalApp/serializers.py
+++ fizzbuzzers/totalApp/serializers.py
@@ -1,6 +1,6 @@
 from .models import fizzBuzz
 from rest_framework import serializers
-
+#Creates the serializer that allows data types to be easily converted
 class fizzBuzzSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = fizzBuzz
